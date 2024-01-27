ae2451c24e7d1cd8ee916947b7496616c2decd58
Supun Mahendra <supunmahendra@gmail.com>
2024-01-27 19:50:14 +0100

    new

---
+++ index.js
@@ -0,0 +1,86 @@
+// alert for JS;
+/*what a good
+comment */
+
+//alert("we are inside the index JS file");
+//alert("second JS file");
+//alert(3+4+2); alert("last");
+//let a = "hello";
+//alert (a);
+
+//let supun;
+//supun = "0505";
+//alert (supun);
+//let pn = 123;
+//console.log(pn);
+
+//let user1 = "supun";
+//let user2 = "salet";
+//let user3 = "maham";
+
+
+
+//let user1 = "supun", user2 = "salet", user3 = "maham";
+
+//var msg = "hello";
+//console.log(msg);
+
+//let student = "kowsar";
+//student = "Jagath";
+//console.log(student);
+
+
+//let topstudent ="lalanka";
+//let student="koswar"
+//student = topstudent;
+//console.log(topstudent);
+
+
+//let user ="you";
+//let user ="me";
+
+//let USER= "hello"
+//let user="hello world"
+
+//let myName;
+
+//let myNameFunctionForm;
+
+//console.log (user);
+
+// constant
+
+//const student = "someone";
+//student = "Me";
+//const MY_STUDENT ="someone";
+//const myStudent ="someine";
+
+//console.log(myStudent);
+
+
+//let myRollNumber = 123;
+//let myBP = 49.58;
+
+//let number1 =10;
+//let number2 =20;
+//console.log (number1+number2);
+
+//let name ="my name is";
+//let name2 = 'lalanke';
+//let name3 = `my name is ${name2}`;
+//console.log(name3);
+
+
+//let number1 = 1;
+//let number2 = 2;
+//console.log(`my roll number is ${number1+number2}`);
+//console.log (number1+name2);
+
+//let number= 8>5;
+//console.log(number);
+
+let number="5";
+let su="6";
+console.log(+su + +number);
+
+S
