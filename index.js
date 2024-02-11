5fb097346addae4168ab2c7a0f5509148f458380
Supun Mahendra <supunmahendra@gmail.com>
2024-02-11 08:12:44 +0100

    20210211

--- index.js
+++ index.js
@@ -79,8 +79,659 @@ comment */
 //let number= 8>5;
 //console.log(number);
 
-let number="5";
-let su="6";
-console.log(+su + +number);
+//let number="5";
+//let su="6";
+//let p=8;
+//let output="su"+p+"number"
 
-S
+//let output=su/number
+//console.log(su+p+number);
+//let output=Number(su);
+//let num = true;
+//let output=Number(num);         
+//console.log(output);
+//console.log(typeof output);
+
+
+
+//let num = 0;
+//let output=Boolean(num);         
+//console.log(output);
+//console.log(typeof output);
+
+//let num = "";
+//let output=Boolean(num);         
+//console.log(output);
+//console.log(typeof output);
+
+//let num1=10;
+//let num2=2;
+//let result= num1 % num2;
+//console.log(result);
+
+//chaining assignment
+//let num1=4;
+//let num2=5;
+//let num3=3;
+//let a,b,c;
+//a=b=c= num2 + num1+ num3;
+//console.log(a);
+//console.log(b);console.log(c);
+
+//let num1=-true;
+
+//console.log(num1);
+
+
+//let string1="1";
+//let string2="3"
+
+//let result=+string1+ +string2;
+//console.log(result)
+
+//let num1 =1;
+//result = num1+num1;
+//result2= result+num1
+
+//console.log(result2);
+
+
+
+//let num1 =5;
+// num1++;
+//console.log(num1);
+
+//let num2 =5;
+// num2--;
+//console.log(num2);
+
+//let num3 =5;
+// result=num3++;
+//console.log(result);
+
+//let num4 =5;
+// result=++num4;
+//console.log(result);
+
+//let num1 =true;
+//let num2 =1;
+//console.log(num1 == num2);
+
+//let string1 ="glaf";
+//let string2 ="gloe";
+//console.log(string1>string2);
+
+//let string1 ="bee";
+//let string2 ="be";
+//console.log(string1>string2);
+
+//let num1 =2;
+//let string2 ="2";
+//console.log(num1==string2);
+
+//let firstValue ="0";
+//console.log(Boolean(firstValue));
+
+//let secondValue =0;
+//console.log(Boolean(secondValue));
+
+//console.log(Boolean(firstValue==secondValue));
+
+//let weather ="muddy";
+//if(weather == "sunny"){
+//    console.log("carry a hat")}
+//else if(weather == "muddy"){
+ //   console.log("carry a boot")}
+//else{ console.log("dont go out")}
+
+
+//let weather ="rin";
+//switch(weather) {
+//    case "snow":
+//        console.log("carry a hat")
+//       break;
+ //   case "rain":
+ //       console.log("carry a bat")
+  //      break;
+   // default:
+   //     console.log("dont go out")   
+//}
+
+
+//let weather ="snow";
+//let temperature=1;
+//let isRaining=false;
+
+
+//if (weather == "sunny" && temperature == 19 && isRaining === false){
+//        console.log("og out") 
+//}
+
+//else if (weather == "snow" && temperature == 1 && isRaining === false){
+//    console.log("og") 
+//}
+//else {
+//console.log ("in")
+//};
+
+
+
+//let weather ="sunny";
+//let temperature=1;
+//let isRaining=false;
+
+
+//if (weather == "sunny" && (temperature >= 15 && temperature <= 25) && isRaining === false){
+//        console.log("og out") 
+//}
+
+//else if (weather == "snow" && temperature == 1 && isRaining === false){
+//    console.log("og") 
+//}
+
+//else {
+//console.log ("in")
+//};
+
+
+
+//let weather ="snow";
+//let temperature=35;
+//let isRaining=false;
+
+
+//if (weather == "sunny" && (temperature >= 15 && temperature <= 36) && isRaining === false){
+   //     console.log("og out") 
+//}
+
+//else if (weather == "snow" && temperature > 30 && !isRaining ){ 
+//    console.log("og") 
+//}
+//con
+//else {
+//console.log ("in")
+//};
+
+
+//let weather = "sunny"
+
+
+
+//if (weather == "sunny") {
+ //   console.log ("hat")
+//}
+
+///if (weather == "rainy") {
+//    console.log ("rain coat")
+//}
+
+//else if (weather == "snow") {
+//    console.log ("jacket")
+//}
+
+//else if (weather == "cloudy") {
+//    console.log ("over coat")
+//}
+
+//else {
+//    console.log ("t-shairt")
+//}
+
+
+/* 
+/* function weatherFinland(weather){
+
+if (weather == "sunny") {
+    console.log ("hat")
+}
+
+if (weather == "rainy") {
+    console.log ("rain coat")
+}
+
+else if (weather == "snow") {
+    console.log ("jacket")
+}
+
+else if (weather == "cloudy") {
+    console.log ("over coat")
+}
+
+else {
+    console.log ("t-shairt")
+}
+}
+
+weatherFinland("snow") */ 
+
+/*function helloWorldNormal (){
+    console.log("hellow world normal")S
+}
+
+const helloWorldArrow = () -> {
+    console.log("hellow world arrow")
+}
+
+helloWorldNormal ()
+helloWorldArrow ()*/
+
+//function tp(name, age, country, city){
+// return{ name, age, country, city}
+//}
+
+//let world = tp("supun", 34, "finland", "tampere")
+//console.log(world)
+
+//console.log((world.name))
+//console.log("my name is "+world.name+" and")
+
+//Loops
+
+//const array = [11,24,53,43,58]
+//console.log(array[2])
+
+
+//let value=""
+//for(let i=0;i<3; i++){
+//    value=+i
+ //   console.log(value)
+ //   console.log(value+". "+array[i])
+//}
+
+//const array = [11,24,53,43,58]
+//console.log(array[2])
+
+
+//let value=""
+//for(let i=0; i<array.length; i++){
+ //   value=+i
+ //   console.log(value)
+ //   console.log(value+1+". "+array[i])
+//}
+ //inctial value condition itaration
+
+
+ //let value=0
+ //let b=""
+ //let c=1000
+//for(let i=3; i<c; i=i+3){
+ //   valueOne+=value+i}
+ //   console.log(valueOne)}
+ //for(let i=5; i<c; i=i+5){
+    //   valueTwo+=value+i}
+    //   console.log(valueTwo)
+//
+ //inctial value condition itaration
+
+ 
+ //const array = ["red","blue","yellow","black","green"]
+ //let dom=document.getElementById("colors")
+ 
+ 
+ //let colors=""
+ //for(let i=0; i<array.length; i++){
+ //    colors +="<li>"+array[i]+"</li>"     
+ //}
+ //.log(colors)
+ //dom.innerHTML = colors
+
+ //let number=""
+ //for(let i=1;i>=33;i--){
+  //  number =i
+  //  console.log(number)
+ //}
+
+ //let saving =0;
+ //let goal =1000;
+//while (saving<goal){
+ //   saving=saving+100
+ //   console.log("current saving is "+saving)
+//}
+
+//let initialNuber = 0;
+//while (initialNuber<100){
+  //  console.log(initialNuber)
+  //  initialNuber ++
+//}
+ 
+//let value=0
+//let c=1000
+//let numberOne=0
+//for(let i=3; i<c; i=i+3){
+//   numberOne= i + numberOne    
+//}
+// for(let i=5; i<c; i=i+5)
+//{ //     numberTwo=i+numberTwo
+//}
+
+
+//1. intriductions to data types
+
+//let num1 = 2;
+//let num2 = 0;
+//let num = 5;
+
+
+//2. basic functions
+
+//function add(num1, num2) {
+//    return ´${num1} plus ${num2} equals ${num1+num2}´;
+//}
+
+//function substract(num1, num2) {
+//    return ´${num1} minus ${num2} equals ${num1-num2}´;
+//}
+
+//function multifly(num1, num2) {
+//    return ´${num1} times ${num2} equals ${num1*num2}´;
+//}
+
+//function devide(num1, num2) {
+//    return ´${num1} devided by ${num2} equals ${num1/num2}´;
+//}
+
+//2. magic function
+
+//function magicOperation(true){
+ //   switch(num){
+ //       case num<10: return "magic dust makes the number grow: " + (num*10);
+ //       case num>=10 && num<=100: return "A magic gone doubles the number: " + (num*2);
+  //      default: return "the magic is too mighty for magic and remains unchanged.";
+ //   }
+//}
+
+//3. Loop Operation
+
+//let continueCalculating = true;
+//while (continueCalculating) {
+//    let operation = prompt ("Choose an operation: add, subtract, multiply, devide, magic or exit");
+//    if (operation === "add"){
+//        
+ //   }
+ //   if (operation === "subtract"){
+//
+ //   }
+ //   if (operation === "multiply"){
+//
+ //   }
+ //   if (operation === "devide"){
+//
+ //   }
+ //   if (operation === "magic"){
+//
+ //   }
+ //   if (operation === "exit"){
+//        console.log ("goodbye!");
+ //       break;
+//    }
+//}
+
+//4. input validation
+
+//let continueCalculating = true;
+//while (continueCalculating) {
+//    let operation = prompt ("Choose an operation: add, subtract, multiply, devide, magic or exit");
+ //   if (operation === "add"){
+//
+ //   }
+ //   if (operation === "subtract"){
+//
+ //   }
+ //   if (operation === "multiply"){
+//
+ //   }
+ //   if (operation === "devide"){
+//
+ //   }
+ //   if (operation === "magic"){
+//
+ //   }
+ //  if (num2 === 0 && operation === "devide"){
+ //       console.log ("can not devide by zero. try another operation");
+ //       continue;
+ //   }
+//
+ //   if (operation === "exit"){
+ //       console.log ("goodbye!");
+ //       break;
+ //   }
+//}
+
+
+
+
+
+/*let userWantsToContinue = prompt ("If you want to continue to the calculator type yes, otherwise type No");
+console.log ("welcome to the magic calculator!");
+
+while (userWantsToContinue === "Yes" || userWantsToContinue === "yes"){
+    
+    let operation = prompt ("Choose an operation: add, subtract, multiply, devide, magic or exit");
+
+    //magic operation
+    if (operation === "magic"){
+        let num = prompt ("please give a number");
+        function magicOperation(num){
+           switch (true) {
+                case num<10 : return "magic dust makes the number grow: " + (num*10);
+                case num>=10 && num<=100 : return "A magic gone doubles the number: " + (num*2);
+                default : return "the magic is too mighty for magic and remains unchanged.";
+            }
+        }
+        console.log(magicOperation(num));
+    }
+
+    //real operation    
+    if (operation === "add" || operation ==="subtract" || operation === "multiply" || operation === "devide"){
+        let num1 = prompt ("Give number 01"); 
+        let num2 = prompt ("Give number 02"); 
+
+        //add
+        function add(num1,num2) {
+            return `${num1} plus ${num2} equals ${+num1+ +num2}`;
+        }
+
+        //substract
+        function subtract(num1,num2) {
+            return `${num1} minus ${num2} equals ${num1 - num2}`;
+        }
+
+        //multiply
+        function multiply(num1,num2) {
+            return `${num1} times ${num2} equals ${num1 * num2}`;
+        }
+
+        //devideyes
+        function devide(num1,num2) {
+            if (num2 == 0 && num1 == 0) {
+                console.log ("can not devide zero by zero. try another operation");
+            }
+            else if (num2 == 0 && operation === "devide") {
+                console.log ("can not devide by zero. try another operation");
+            }
+            if (!(num2 === 0)){
+                return `${num1} devide by ${num2} equals ${num1 / num2}`;
+             }
+        }
+
+        switch (operation) {
+           case "add" : 
+               console.log (add(num1,num2));
+               break;
+            case "subtract" :
+               console.log (subtract(num1,num2));
+               break;
+            case "multiply" : 
+               console.log (multiply(num1,num2));
+               break;
+            case "devide" : 
+               console.log (devide(num1,num2));
+               break;
+           default: 
+               console.log ("Invalid operation, Please try again. sdff");
+               break;
+        }   
+    }
+    
+    if(operation === "exit"){
+        console.log ("Thank you, goodbye!");
+        break;
+    }
+}
+
+if (userWantsToContinue === "No" || userWantsToContinue === "no" ){
+    console.log ("Thank you, goodbye!");
+}
+*/
+
+//add somting to end array
+/*let vgArray = ["banana", "potato" , "onoin"];
+vgArray.push ("chicken", "fish")
+console.log(vgArray)*/
+
+//remove from end
+/*let vgArray = ["banana", "potato" , "onoin"];
+console.log("before the array length " +vgArray.length)
+vgArray.pop ()
+console.log(vgArray)
+console.log("after the array length " +vgArray.length)*/
+
+//add from begining
+/*let vgArray = ["banana", "potato" , "onoin"];
+console.log("before the array length " +vgArray.length)
+vgArray.unshift ("chicken", "fish")
+
+console.log("after the array length " +vgArray.length)
+console.log(vgArray)*/
+
+
+//remove from begining
+/*let vgArray = ["banana", "potato" , "onoin"];
+console.log("before the array length " +vgArray.length)
+vgArray.shift ()
+
+console.log("after the array length " +vgArray.length)
+console.log(vgArray)*/
+
+//remove from array
+/*let vgArray = ["banana", "potato" , "onoin" ,"chicken", "fish"];
+console.log("before the array length " +vgArray.length)
+vgArray.splice(2,2)
+
+console.log("after the array length " +vgArray.length)
+console.log(vgArray)*/
+
+
+//replace to array
+/*let vgArray = ["banana", "potato" , "onoin" ,"chicken", "fish"];
+console.log("before the array length " +vgArray.length)
+vgArray.splice(1,2, "item", "iten2")
+
+console.log("after the array length " +vgArray.length)
+console.log(vgArray)
+**/
+
+//replace to array
+/*let vgArray = ["banana", "potato" , "onoin" ,"chicken", "fish"];
+console.log("before the array length " +vgArray.length)
+sliceArr=vgArray.slice(1,4)
+
+console.log("after the array length " +vgArray.length)
+console.log(sliceArr)*/
+
+
+//merge two arrays
+/*let vgArray = ["banana", "potato" , "onoin" ,"chicken", "fish"];
+let vgArrayDup = ["test1", "test2" , "test3"];
+console.log("before the array length " +vgArray.length)
+let mergArr= vgArrayDup.concat(vgArray)
+
+console.log("after the array length " +vgArray.length)
+console.log(mergArr)*/
+
+
+//take the place of index
+/*let vgArray = ["banana", "potato" , "onoin" ,"chicken", "fish"];
+
+let mergArr= vgArray.indexOf("potato")
+
+console.log("after the array length " +vgArray.length)
+console.log(mergArr)
+**/
+
+//take the place of index
+/*let vgArray = ["banana", "potato" , "onoin" ,"chicken","potato", "fish"];
+
+let newgArr= vgArray.IndexOf("potato")
+let mergArr= vgArray.lastIndexOf("potato")
+
+console.log("after the array length " +vgArray.length)
+console.log(newgArr)
+console.log(mergArr)*/
+
+// se weather it is there
+/*let vgArray = ["banana", "potato" , "onoin" ,"chicken", "fish"];
+let vgArrayDup = ["test1", "test2" , "test3"];
+
+
+let includeExamp = vgArray.includes("potato")
+console.log(includeExamp)*/
+
+
+/*let vgArray = ["banana", "potato" , "onoin" ,"chicken", "fish"];
+let vgArrayDup = ["test1", "test2" , "test3"];
+let num =[1,2,3,4,5]
+
+
+let includeExampNum = num.forEach(console.log)
+
+console.log(includeExampNum)*/
+
+
+/*let vgArray = ["banana", "potato" , "onoin" ,"chicken", "fish"];
+let vgArrayDup = ["test1", "test2" , "test3"];
+let num =[1,2,3,4,5]
+
+const test = (a,b,c) => {
+    return a+b+c
+}
+
+console.log(test(1, 5, 6) )*/
+
+/*let vgArray = ["banana", "potato" , "onoin" ,"chicken", "fish"];
+let vgArrayDup = ["test1", "test2" , "test3"];
+let num =[1,2,3,4,5]
+
+function each(currentValue){
+    console.log(currentValue)
+}
+
+let foreach = num.forEach(each)
+
+console,log(foreach)*/
+
+
+/*let vgArray = ["banana", "potato" , "onoin" ,"chicken", "fish"];
+let vgArrayDup = ["test1", "test2" , "test3"];
+let num =[1,2,3,4,5]
+
+function each(currentValue, dex, arr){
+    console.log(dex)
+}
+
+let foreach = num.forEach(each)
+
+console,log(foreach)*/
+
+/*vgArray = ["banana", "potato" , "onoin" ,"chicken", "fish"];
+
+vgArray.push("diana")
+console.log(vgArray)
+
+let arr1=vgArray.indexOf("onoin")
+console.log(arr1)
+
+arr2=vgArray.splice(arr1,1)
+console.log(vgArray)*/
+
+
